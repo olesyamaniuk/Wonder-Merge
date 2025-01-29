02035ed06b01abcb78851358a83bdef5db3468b9
olesyamaniuk <maniukolesya222@gmail.com>
2025-01-29 16:08:24 +0200

    2

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -26,10 +26,10 @@ function toggleAnswer(element) {
 
   if (answer.style.display === "none" || answer.style.display === "") {
     answer.style.display = "block";
-    buttonImg.src = "/img/faq/arrow.png"; 
+    buttonImg.src = "/image/faq/Frame 531.png"; 
   } else {
     answer.style.display = "none";
-    buttonImg.src = "/img/faq/icon.png"; 
+    buttonImg.src = "/image/faq/Frame 530.png"; 
   }
 }
 
